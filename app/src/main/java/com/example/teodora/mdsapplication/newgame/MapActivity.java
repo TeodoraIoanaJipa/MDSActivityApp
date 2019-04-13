498c143cc5e251ef1c6f4af9e14a86af508f9d27
Jipa Teodora <jipa.ioana.teodora@gmail.com>
2019-04-13 13:59:56 +0300

    added mapActivity

---
+++ app/src/main/java/com/example/teodora/mdsapplication/newgame/MapActivity.java
@@ -0,0 +1,15 @@
+package com.example.teodora.mdsapplication.newgame;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.teodora.mdsapplication.R;
+
+public class MapActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+    }
+}
