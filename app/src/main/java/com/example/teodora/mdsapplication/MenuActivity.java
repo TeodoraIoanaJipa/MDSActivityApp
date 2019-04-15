69e6e56934766561f2c0e4d2aa0ee2eecb7b33da
Andrei Tudorache <32478989+Andrei2D@users.noreply.github.com>
2019-04-15 18:01:53 +0300

    NewGame + Map + TimeCard + PopUpInfos (#3)
    * Modified New Game Activity
    * It it's made of Contraint now
    * Put buttons for back and continue
    * Added input text for team name
    * Added buttons for pawn color selection
    * Moved the team members input and list on the bottom
    * Changed from snake_case to camelCase
    * Added comms in new_game.xml
    
    * Updated demo files
    Added demo for v0.2 of New game
    Modified README.md
    
    * Modified New Game Activity
    Added model related classes and changed the appearance of the layout
    
    * Modified New Game Activity
    * Created multiple auxiliar classes
    * Changed background color and apperances
    * Added mutiple storing functionalities
    * Must resolve non-consistent pawn-blocking bug
    * Must resolve non-clickable pawns buttons
    * Must resolve reset activity bug trigger on backBtn
    * Must resolve team import between teams screens
    * Must reset all teams models on TeamsNumber Activity
    
    * Done New Game Activity

--- app/src/main/java/com/example/teodora/mdsapplication/MenuActivity.java
+++ app/src/main/java/com/example/teodora/mdsapplication/MenuActivity.java
@@ -5,14 +5,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
 import android.view.View;
-import android.widget.Button;
 
 import com.example.teodora.mdsapplication.leaderboard.LeaderboardActivity;
-import com.example.teodora.mdsapplication.newgame.NewGameActivity;
+import com.example.teodora.mdsapplication.newgame.TeamsNumber;
 
 public class MenuActivity extends AppCompatActivity {
 
-    private CardView new_game_btn;
+    private CardView teamNumbersCardView;
     private CardView leaderboardCardView;
 
     @Override
@@ -20,16 +19,16 @@ public class MenuActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
 
-        new_game_btn = (CardView) findViewById(R.id.new_game_card_view);
-        new_game_btn.setOnClickListener(new View.OnClickListener() {
+        teamNumbersCardView = (CardView) findViewById(R.id.newGameCardView);
+        teamNumbersCardView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), NewGameActivity.class);
+                Intent intent = new Intent(getApplicationContext(), TeamsNumber.class);
                 startActivity(intent);
             }
         });
 
-        leaderboardCardView = (CardView) findViewById(R.id.leaderboardcardView);
+        leaderboardCardView = (CardView) findViewById(R.id.leaderBoardCardView);
         leaderboardCardView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
